6e80f5d6c0e45b7e015883b7f54af3404b039006
Daniel Legan <legan.daniel@gmail.com>
2022-09-01 14:17:34 -0500

    completed stretch goals

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -98,9 +98,12 @@ console.log('The favorite foods are now', favoriteFoods);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+console.log(favoriteFoods.join(' and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let newArray = favoriteFoods.concat(animalArray)
+console.log(newArray);
